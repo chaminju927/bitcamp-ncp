19a2d6c9c1f2e9b0d05b525f76582cbe1bcb57fc
chaminju927 <minjoo92718@gmail.com>
2023-01-12 00:55:32 +0900

    .

--- backend-app/app/src/main/java/bitcamp/boot/app/controller/TeacherController.java
+++ backend-app/app/src/main/java/bitcamp/boot/app/controller/TeacherController.java
@@ -3,6 +3,7 @@ package bitcamp.boot.app.controller;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,44 +11,29 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import bitcamp.boot.app.dao.TeacherDao;
 import bitcamp.boot.app.vo.Teacher;
 
-@RestController
 @CrossOrigin(origins = {"http://127.0.0.1:5500", "http://localhost:5500"})
+@RestController
 public class TeacherController {
 
   TeacherDao teacherDao = new TeacherDao();
 
-  @PostMapping("/teachers")     //스프링부트가 호출하는 메서드
-  public Object addTeacher(Teacher teacher
-      //      String name,
-      //      String tel,
-      //      String email,
-      //      String academicLevel,
-      //      String university,
-      //      String major,
-      //      int pay
-      ) {
+  @PostMapping("/teachers")
+  public Object addTeacher(Teacher teacher) {
 
-    Teacher t = new Teacher();
-    t.setName(name);
-    t.setTel(tel);
-    t.setEmail(email);
-    t.setAcademicLevel(academicLevel);
-    t.setUniversity(university);
-    t.setMajor(major);
-    t.setPay(pay);
+    teacher.setCreatedDate(new Date(System.currentTimeMillis()).toString());
 
-    t.setCreatedDate(new Date(System.currentTimeMillis()).toString());
-
-    this.teacherDao.insert(t);
+    this.teacherDao.insert(teacher);
 
     Map<String,Object> contentMap = new HashMap<>();
-    contentMap.put("status","success");
+    contentMap.put("status", "success");
 
     return contentMap;
   }
+
 
   @GetMapping("/teachers")
   public Object getTeachers() {
@@ -55,24 +41,28 @@ public class TeacherController {
     Teacher[] teachers = this.teacherDao.findAll();
 
     Map<String,Object> contentMap = new HashMap<>();
-    contentMap.put("status","success");
-    contentMap.put("data",teachers);
+    contentMap.put("status", "success");
+    contentMap.put("data", teachers);
 
     return contentMap;
   }
 
+
   @GetMapping("/teachers/{no}")
   public Object getTeacher(@PathVariable int no) {
-    Teacher t = this.teacherDao.findByNo(no);
+
+    Teacher b = this.teacherDao.findByNo(no);
+
     Map<String,Object> contentMap = new HashMap<>();
 
-    if (t == null) {
-      contentMap.put("status","failure");
+    if (b == null) {
+      contentMap.put("status", "failure");
       contentMap.put("data", "강사가 없습니다.");
     } else {
-      contentMap.put("status","success");
-      contentMap.put("data", t);
+      contentMap.put("status", "success");
+      contentMap.put("data", b);
     }
+
     return contentMap;
   }
 
@@ -82,41 +72,37 @@ public class TeacherController {
     Map<String,Object> contentMap = new HashMap<>();
 
     Teacher old = this.teacherDao.findByNo(teacher.getNo());
-    if (old == null ) {
+    if (old == null) {
       contentMap.put("status", "failure");
       contentMap.put("data", "강사가 없습니다.");
       return contentMap;
     }
+
     teacher.setCreatedDate(old.getCreatedDate());
 
     this.teacherDao.update(teacher);
 
-    contentMap.put("status","success");
+    contentMap.put("status", "success");
+
     return contentMap;
   }
 
+  @DeleteMapping("/teachers/{no}")
+  public Object deleteTeacher(@PathVariable int no) {
 
-  @DeleteMapping("/teachers/{teacherNo}")
-  public Object deleteTeacher(@PathVariable int teacherNo) {
-
-    Teacher t = this.teacherDao.findByNo(teacherNo);
+    Teacher m = this.teacherDao.findByNo(no);
 
     Map<String,Object> contentMap = new HashMap<>();
 
-    if (t == null ) {
-      contentMap.put("status","failure");
-      contentMap.put("data","강사가 없거나 암호가 맞지 않습니다.");
+    if (m == null) {
+      contentMap.put("status", "failure");
+      contentMap.put("data", "강사가 없습니다.");
+
     } else {
-      this.teacherDao.delete(t);
-      contentMap.put("status","success");
+      this.teacherDao.delete(m);
+      contentMap.put("status", "success");
     }
+
     return contentMap;
   }
-
-
-
-
-
-
-
 }
--- backend-app/app/src/main/java/bitcamp/boot/app/dao/TeacherDao.java
+++ backend-app/app/src/main/java/bitcamp/boot/app/dao/TeacherDao.java
@@ -1,8 +1,8 @@
 package bitcamp.boot.app.dao;
 
 import java.util.Arrays;
-import bitcamp.boot.app.vo.Teacher;
 
+import bitcamp.boot.app.vo.Teacher;
 
 public class TeacherDao {
   private static final int SIZE = 100;
@@ -11,26 +11,13 @@ public class TeacherDao {
   private int count;
   private Teacher[] teachers = new Teacher[SIZE];
 
-
-
   public void insert(Teacher teacher) {
     teacher.setNo(++no);
     this.teachers[this.count++] = teacher;
   }
 
   public Teacher[] findAll() {
-    return Arrays.copyOf(teachers,count);
-  }
-
-  public void delete(Teacher teacher) {
-    for (int i = this.indexOf(teacher) + 1; i < this.count; i++) {
-      this.teachers[i - 1] = this.teachers[i];
-    }
-    this.teachers[--this.count] = null; // 레퍼런스 카운트를 줄인다.
-  }
-
-  public void update(Teacher teacher) {
-    this.teachers[this.indexOf(teacher)] = teacher;
+    return Arrays.copyOf(teachers, count);
   }
 
   public Teacher findByNo(int no) {
@@ -42,7 +29,18 @@ public class TeacherDao {
     return null;
   }
 
-  int indexOf(Teacher teacher) {
+  public void update(Teacher teacher) {
+    this.teachers[this.indexOf(teacher)] = teacher;
+  }
+
+  public void delete(Teacher teacher) {
+    for (int i = this.indexOf(teacher) + 1; i < this.count; i++) {
+      this.teachers[i - 1] = this.teachers[i];
+    }
+    this.teachers[--this.count] = null; // 레퍼런스 카운트를 줄인다.
+  }
+
+  private int indexOf(Teacher teacher) {
     for (int i = 0; i < this.count; i++) {
       if (this.teachers[i].getNo() == teacher.getNo()) {
         return i;
@@ -50,6 +48,4 @@ public class TeacherDao {
     }
     return -1;
   }
-
-
 }
